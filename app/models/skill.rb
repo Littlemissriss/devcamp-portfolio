0154d39aed83e2b301c0a525e1c6cae31eae38fe
Littlemissriss <littlemissriss@gmail.com>
2017-07-12 23:41:14 +0000

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,11 +1,4 @@
 class Skill < ApplicationRecord
-  include Placeholder
-  validates_presence_of :title
+  validates_presence_of :title, :percent_utilized
   
-  after_initialize :set_defaults
-  
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
-    self.percent_utilized ||= 50
-  end
 end
