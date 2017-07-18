e2d9a5cfc193699d49782d0f2417edfe908f1c6d
Littlemissriss <littlemissriss@gmail.com>
2017-07-18 17:43:47 +0000

    Implemented redirect for blog show page when non admin tries to access draft

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,8 +11,7 @@ class BlogsController < ApplicationController
   def index
     if logged_in?(:site_admin)
       @blogs = Blog.all.order(updated_at: :desc).page(params[:page]).per(5)
-    end
-    if !logged_in?(:site_admin) 
+    else
       @blogs = Blog.where("status = 1").order(updated_at: :desc).page(params[:page]).per(5)
     end
     
@@ -23,11 +22,15 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page."
+    end
   end
 
   # GET /blogs/new
